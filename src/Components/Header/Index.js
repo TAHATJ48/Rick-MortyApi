9c52c635b444557789ee3d479e1f576f2d81178b
TAHATJ48 <thetotoj@gmail.com>
2022-12-04 21:24:11 +0100

    routingv2

--- src/Components/Header/Index.js
+++ src/Components/Header/Index.js
@@ -7,20 +7,20 @@ const Header = () => {
     
 <nav class="dark:bg-gray-800 rounded ">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <Link className="link" to="/">
+    <Link className="link" to="Rick-MortyApi">
     <img src="https://img.icons8.com/color/96/null/rick-sanchez.png"/>
     </Link>
       <div class="" id="navbar-default">
       <ul class="flex flex-row  ">
         <li>
-        <Link className="m-16" to="/characters">
+        <Link className="m-16" to="/Rick-MortyApi/characters">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         R&M Stats
         </button>
         </Link>
         </li>
         <li>
-        <Link className="m-16 bg-" to="/random">
+        <Link className="m-16 bg-" to="/Rick-MortyApi/random">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Random  
         </button>
